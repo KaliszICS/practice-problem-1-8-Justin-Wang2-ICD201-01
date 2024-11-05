2ec44246f7285976cfce461c7e870f719aeb976b
Justin-Wang2-ICD201-01 <justin.wang@student.tdsb.on.ca>
2024-11-04 20:58:31 -0500

    Update PP1_8.py

--- PP1_8.py
+++ PP1_8.py
@@ -1,18 +1,28 @@
 
 def q1():
-  #Write Assignment code here
-
+  bool1 = True
+  bool2 = False
+  print(bool1 and bool2)
+  print(bool1 or bool2)
 def q2():
-  #Write Assignment code here
+  inp = input("Enter an integer: ")
+  inp = int(inp)
+  print(0 < inp > 0)
 
 def q3():
-  #Write Assignment code here
-
+  num = input("Enter a number: ")
+  num = int(num)
+  print(0 <= num <= 10)
 def q4():
-  #Write Assignment code here
+  food = input("Input food: ")
+  drink = input("Input drink: ")
+  print("pizza" == food and drink == "pop")
 
 def q5():
-  #Write Assignment code here
+  num = input("Enter an integer: ")
+  num = int(num)
+  tof = num % 2 == 0
+  print(f"The integer {num} is {tof}.")
 
 #Do not edit code after this
 #Comment out the followwing code when running tests
