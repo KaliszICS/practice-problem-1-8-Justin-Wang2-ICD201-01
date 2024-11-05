3e3d805a68e4d2c005b8c7b32a06ff93e0eb89e8
Justin-Wang2-ICD201-01 <justin.wang@student.tdsb.on.ca>
2024-11-05 14:55:11 -0500

    Update PP1_8.py

--- PP1_8.py
+++ PP1_8.py
@@ -16,7 +16,7 @@ def q3():
 def q4():
   food = input("Input food: ")
   drink = input("Input drink: ")
-  print("pizza" == food and drink == "pop")
+  print(not food == "pizza" or not drink == "drink")
 
 def q5():
   num = input("Enter an integer: ")
@@ -25,7 +25,7 @@ def q5():
   print(f"The integer {num} is {tof}.")
 
 #Do not edit code after this
-#Comment out the followwing code when running tests
+#Comment out the following code when running tests
 
 #q1()
 #q2()
